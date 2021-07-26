6823a6303bd88c32d958d2579ab1dc346172b7fc
van-thong <9thong_le@singaporeair.com.sg>
2021-07-26 23:35:52 +0700

    hic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,34 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// import Anime from 'react-anime'
 import { useRef, useEffect } from 'react'
-import anime, { AnimeInstance } from 'animejs'
+// import anime, { AnimeInstance } from 'animejs'
 
 export default function Home() {
-  const animeRef = useRef<AnimeInstance>()
+  // const animeRef = useRef<HTMLHeadingElement>(null)
 
-  useEffect(() => {
-    animeRef.current = anime
-      .timeline({ loop: true })
-      .add({
-        targets: '.ml2 .letter',
-        scale: [4, 1],
-        opacity: [0, 1],
-        translateZ: 0,
-        easing: 'easeOutExpo',
-        duration: 950,
-        delay: (el, i) => 70 * i,
-      })
-      .add({
-        targets: '.ml2',
-        opacity: 0,
-        duration: 1000,
-        easing: 'easeOutExpo',
-        delay: 1000,
-      })
-  }, [])
+  // useEffect(() => {
+  //   if (animeRef && animeRef.current) {
+  //     animeRef.current = anime
+  //       .timeline({ loop: true })
+  //       .add({
+  //         targets: '.ml2 .letter',
+  //         scale: [4, 1],
+  //         opacity: [0, 1],
+  //         translateZ: 0,
+  //         easing: 'easeOutExpo',
+  //         duration: 950,
+  //         delay: (el, i) => 70 * i,
+  //       })
+  //       .add({
+  //         targets: '.ml2',
+  //         opacity: 0,
+  //         duration: 1000,
+  //         easing: 'easeOutExpo',
+  //         delay: 1000,
+  //       })
+  //   }
+  // }, [])
 
   return (
     <div className={styles.container}>
@@ -40,7 +42,7 @@ export default function Home() {
         <h1 className={`${styles.title} ${styles.heroGradientHeading}`}>
           Van Thong Lee
         </h1>
-        <h1 className="ml2" ref={animeRef}>
+        <h1 className="ml2">
           {'Comming Soon...'.split('').map((v, index) => (
             <span className="letter" key={index}>
               {v}
