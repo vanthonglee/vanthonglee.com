74f83c0da51403d483443bb833d619ef329c65cc
van-thong <9thong_le@singaporeair.com.sg>
2021-07-26 22:23:26 +0700

    📕update content at homepage 'comming soon...'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRef, useEffect } from 'react'
+import anime, { AnimeInstance } from 'animejs'
 
 export default function Home() {
+  const animeRef = useRef<AnimeInstance>()
+
+  useEffect(() => {
+    animeRef.current = anime
+      .timeline({ loop: true })
+      .add({
+        targets: '.ml2 .letter',
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: 'easeOutExpo',
+        duration: 950,
+        delay: (el, i) => 70 * i,
+      })
+      .add({
+        targets: '.ml2',
+        opacity: 0,
+        duration: 1000,
+        easing: 'easeOutExpo',
+        delay: 1000,
+      })
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +39,13 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={`${styles.title} ${styles.heroGradientHeading}`}>
           Van Thong Lee
+        </h1>
+        <h1 className="ml2" ref={animeRef}>
+          {'Comming Soon...'.split('').map((v, index) => (
+            <span className="letter" key={index}>
+              {v}
+            </span>
+          ))}
         </h1>
       </main>
     </div>
