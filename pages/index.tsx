fd4dbb0a185ecbb602250b072fe048c05e04539a
van-thong <9thong_le@singaporeair.com.sg>
2021-07-26 21:33:29 +0700

    update content 'vanthonglee' at homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={`${styles.title} ${styles.heroGradientHeading}`}>
-          Van Thong Le
+          Van Thong Lee
         </h1>
       </main>
     </div>
